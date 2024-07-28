f421cba5ba57c36b1b68920ef6a9d9feafcd5603
thekimwatt <71195725+thekimwatt@users.noreply.github.com>
2024-07-28 14:04:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# coding-and-cocktails-lounge
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/thekimwatt/coding-and-cocktails-lounge)
---
+++ counter.js
@@ -0,0 +1,6 @@
+//Import stylesheet
+import './style.css';
+
+//Write Javascript code!
+const appDiv = document.getElementByID('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`; 
